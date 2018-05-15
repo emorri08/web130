4eb8dc6438ac9328835835ccd0bd620050bd36a0
Elly <emorri08@seattlecentral.edu>
2018-05-15 02:14:51 -0700

    Add files via upload

--- eboyd-spr18-a03/js/build/app.js
+++ eboyd-spr18-a03/js/build/app.js
@@ -10365,26 +10365,24 @@ return jQuery;
 
 /* global $ */
 
-class Pumpkin {
-    constructor(pie) {
-        console.log('You ate ' + pie + ' pie.');
-    }
-}
-
-let pumpkin = new Pumpkin('banana cream');
-
 $(document).ready(function () {
-    let menu = $('nav > ul.closed'),
+    let menu = $('nav > ul'),
         menuLink = $('#mobile-menu a');
 
     menuLink.on('click', function () {
         if (menu.hasClass('closed')) {
             menu.removeClass('closed');
             menu.addClass('open');
-            //            alert('AWW Bitch! You clicked the clicky thing');
         } else {
             menu.removeClass('open');
             menu.addClass('closed');
         }
+    });
+
+    let subMenu = $('.submenu'),
+        subMenuLink = $('.submenu-link');
+
+    subMenuLink.on('click', function () {
+        $(this).parent().find('.submenu').toggle();
     });
 });
